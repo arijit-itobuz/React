ec0e86a34fb2c72cb4f76189153ef60e0b8a6a91
APPLE <apple@APPLEs-MacBook-Air.local>
2022-07-12 17:49:42 +0530

    new-files

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -4,15 +4,16 @@ import NewsItem from './NewsItem'
 export default class News extends Component {
   render() {
     return (
-        <div className='container my-3 p-3 border border-dark rounded-3 d-flex justify-content-center align-items-center'>
-          <NewsItem title="demo-title" description="demo-description"/>
-          <NewsItem title="demo-title" description="demo-description"/>
-          <NewsItem title="demo-title" description="demo-description"/>
-          <NewsItem title="demo-title" description="demo-description"/>
-          <NewsItem title="demo-title" description="demo-description"/>
-          <NewsItem title="demo-title" description="demo-description"/>
-          <NewsItem title="demo-title" description="demo-description"/>
-        </div>      
+      <div className='container my-3 p-3 border border-dark rounded-3'>
+        <h1 className='mb-3 fw-bold'>Top Headlines - Explore</h1>
+        <div className='d-flex justify-content-center align-items-start flex-wrap'>
+          <NewsItem title="" description="" imgUrl="" />
+          <NewsItem title="" description="" />
+          <NewsItem title="" description="" />
+          <NewsItem title="" description="" />
+        </div>
+      </div>
+
     )
   }
 }
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -2,18 +2,16 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
-    let {title, description} = this.props
+    let { title, description, imgUrl } = this.props
     return (
-      <>
-        <div className="card" style={{width: "20rem"}}>
-          <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">{title}</h5>
-              <p className="card-text">{description}</p>
-              <a href="/" className="btn btn-primary">Go somewhere</a>
-            </div>
+      <div className="card m-2" style={{width: "20rem"}}>
+        <img src={imgUrl} className="card-img-top" alt={`img-${title}`} />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <a href="/" className="btn btn-sm btn-dark">Read More</a>
         </div>
-      </>
+      </div>
     )
   }
 }
